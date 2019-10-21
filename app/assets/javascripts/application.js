0de2c7e12b2950223c8578ec1416d742c8383f52
Diana Ambrosie <dianaambrosie@gmail.com>
2019-10-21 01:50:57 +0000

    added bootstrap to app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,11 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require 
+//= require bootstrap-sprockets
+//= require tether
+//= require popper
 //= require_tree .
+
